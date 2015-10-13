e6e23ee9295aacec10df5c3a6fd13ac65399dc8b
hariv <hariv2992@gmail.com>
2015-10-14 04:39:52 +0530

    Modularized file operations and account info

--- module/bin/main.js
+++ module/bin/main.js
@@ -6,11 +6,60 @@ var fs = require('fs');
 var argv = process.argv.slice(2);
 var command = argv[0];
 var token = argv[1];
+
+function DropboxClient(token) {
+    this.token = token;
+    this.fileOperator = new FileOperations(this.token);
+    this.accountInformant = new AccountInformation(this.token);
+}
+
+function AccountInformation(token) {
+    this.token = token;
+    this.url = 'https://api.dropboxapi.com/1/account/info';
+    
+    this.getAccountInfo = function() {
+	request.get(this.url, {
+	    headers: {Authorization: 'Bearer '+this.token}}, function(error, response, data){	
+		if(error) {
+		    console.log('Error '+error);
+		    return;
+		}
+		console.log(response['body']);
+	    });
+    };
+}
+
+function FileOperations(token) {
+    this.token = token;
+    this.url = 'https://api.dropbox.com/1/fileops';
+    this.formData = new Object();
+ 
+    this.performOperation = function(action, root, path1, path2) {
+	this.formData.root = root;
+	if(path2) {
+	    this.formData.from_path = path1;
+	    this.formData.to_path = path2;
+	}
+	else {
+	    this.formData.path = path1;
+	}
+	request.post(this.url+'/'+action, {
+	    headers: {Authorization: 'Bearer '+this.token}, formData: this.formData}, function(error, response, data){
+		if(error) {
+		    console.log("Error "+error);
+		    return;
+		}
+		console.log(action+' done');
+		console.log(response['body']);
+	    });
+    }
+}
+var dropboxClient = new DropboxClient(token);
 if(command === 'authorize') {
     authorize();
 }
 else if(command === 'info') {
-    getAccountInfo();
+    dropboxClient.accountInformant.getAccountInfo();
 }
 else if(command === 'getFile') {
     getFile();
@@ -22,82 +71,16 @@ else if(command === 'metadata') {
     getMetadata();
 }
 else if(command === 'copyFile') {
-    copyFile();
+    dropboxClient.fileOperator.performOperation('copy',argv[2], argv[3], argv[4]);
 }
 else if(command === 'createFolder') {
-    createFolder();
+    dropboxClient.fileOperator.performOperation('create_folder',argv[2], argv[3]);
 }
 else if(command === 'deleteFile') {
-    deleteFile();
+    dropboxClient.fileOperator.performOperation('delete', argv[2], argv[3]);
 }
 else if(command === 'moveFile') {
-    moveFile();
-}
-
-function moveFile() {
-    var formData = {
-	root: 'auto',
-	from_path: argv[2],
-	to_path: argv[3]
-    };
-    request.post('https://api.dropboxapi.com/1/fileops/move', {
-	headers: {Authorization: 'Bearer '+token}, formData: formData}, function(error, response, data){
-	    if(error) {
-		console.log("Error: "+error);
-		return;
-	    }
-	    console.log("Moved");
-	    console.log(response['body']);
-	});
-    
-}
-
-function deleteFile() {
-    var formData = {
-	root: 'auto',
-	path: argv[2]
-    };
-    request.post('https://api.dropboxapi.com/1/fileops/delete', {
-	headers: {Authorization: 'Bearer '+token}, formData: formData}, function(error, response, data){
-	    if(error) {
-		console.log("Error: "+error);
-		return;
-	    }
-	    console.log("Deleted file");
-	    console.log(response['body']);
-	});
-}
-function createFolder() {
-    var formData = {
-	root: 'auto',
-	path: argv[2]
-    };
-    request.post('https://api.dropboxapi.com/1/fileops/create_folder', {
-	headers: {Authorization: 'Bearer '+token}, formData: formData}, function(error, response, body){
-	    if(error) {
-		console.log("Error: "+error);
-		return;
-	    }
-	    console.log("Created Folder");
-	    console.log(response['body']);
-	});
-}
-
-function copyFile() {
-    var formData = {
-	root: 'auto',
-	from_path: argv[2],
-	to_path: argv[3]
-    };
-    request.post('https://api.dropboxapi.com/1/fileops/copy', {
-	headers: {Authorization: 'Bearer '+token}, formData: formData}, function(error, response, body){
-	    if(error) {
-		console.log("Error: "+error);
-		return;
-	    }
-	    console.log("Copied");
-	    console.log(response['body']);
-	});
+    dropboxClient.fileOperator.performOperation('move',argv[2], argv[3], argv[4]);
 }
 
 function getMetadata() {
@@ -145,17 +128,6 @@ function getFile() {
 	    console.log("Downloaded file");
 	    console.log(response['body']);
 	});
-    });
-}
-function getAccountInfo() {
-    request.get('https://api.dropboxapi.com/1/account/info',{
-	headers: {Authorization: 'Bearer ' + token},
-    }, function(error, response, body) {
-	if (error) {
-	    console.log("Error: "+error);
-	    return;
-	}
-	console.log(response['body']);
     });
 }
 
